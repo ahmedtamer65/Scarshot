b08f3beffd8770e26fa2cf4a867e9758aad6e94b
ahmedtamer65 <40834972+ahmedtamer65@users.noreply.github.com>
2019-09-28 11:27:56 -0400

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,26 @@
+#!bin/bash
+
+pkg update
+clear
+sleep 0.3
+pkg upgrade
+clear
+sleep 0.3
+apt update && apt upgrade
+clear
+sleep 0.3
+pkg install curl
+clear
+sleep 0.3
+pkg install git
+clear
+sleep 0.3
+pkg install php
+clear
+sleep 0.3
+pkg install openssh
+clear 
+sleep 0.3
+pkg install wget
+clear
+bash scarshot.sh
